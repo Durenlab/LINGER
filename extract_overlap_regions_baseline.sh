ca44eeb1748635c3842bbc0c6f234ec772555dc8
qyyuan <43508437+amssyqy@users.noreply.github.com>
2023-09-22 18:43:23 -0400

    Update extract_overlap_regions_baseline.sh

--- extract_overlap_regions_baseline.sh
+++ extract_overlap_regions_baseline.sh
@@ -2,6 +2,7 @@ Input_dir=$1
 GRNdir=$2
 genome=$3
 cd $Input_dir
+cat ATAC.txt|cut -f 1 |sed '1d' |sed 's/:/\t/g'| sed 's/-/\t/g' > Region.bed
 ml bedtools2/2.26.0-gcc/9.5.0
 for i in $(seq 1 22); do
 bedtools intersect -a "$GRNdir/${genome}_Peaks_chr$i.bed" -b Region.bed -wa -wb > "Region_overlap_chr$i.bed"
